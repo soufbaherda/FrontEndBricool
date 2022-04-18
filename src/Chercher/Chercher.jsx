f1ff2e1a42eb775bb77b79756bdbd99d55e80075
MedAmineELABAYA <86174017+MedAmineELABAYA@users.noreply.github.com>
2022-04-18 18:58:44 +0000

    Add files via upload

---
+++ src/Chercher/Chercher.jsx
@@ -0,0 +1,183 @@
+import React, {useState} from "react";
+import "./chercher.css";
+import sch from "./Ressources/Search.png"
+import sh from "./Ressources/Sh.png"
+import DetailModal from "./DetailModal/DetailModal";
+import {styled, TextField} from "@mui/material";
+
+
+function Chercher() {
+    const [data, setData] = useState([]);
+    const [tres,setTres] = useState("");
+    const [nres, setNres] = useState("");
+    const [modalShow, setModalShow] = useState(false);
+    const [user, setUser] = useState(false);
+    const [i,setI]=useState({})
+    var today = new Date();
+    var mindate = today.getFullYear()+"-"+(today.getMonth()+1)+"-"+today.getDate();
+
+    const SearchTextField = styled(TextField)({
+        '& label.Mui-focused': {
+            color: '#ff8800',
+        },
+        '& .MuiInput-underline:after': {
+            borderBottomColor: '#ff8800',
+        },
+        '& .MuiOutlinedInput-root': {
+            '& fieldset': {
+                borderColor: '#131313',
+            },
+            '&:hover fieldset': {
+                borderColor: '#894E0A',
+            },
+            '&.Mui-focused fieldset': {
+                borderColor: '#ff8800',
+            },
+        },
+    });
+
+ /*   const setPick = (x) =>{
+        if(x==="Livraison") return del
+        else if(x==="Plombier") return plb
+        else if(x==="Chhiwat") return cook
+        else if(x==="Eléctricien") return elect
+        else if(x==="Nettoyage") return cln
+        else return others
+    }*/
+    const rafraichir = () => {
+
+
+        var keywords = document.getElementById("searchbar").value;
+        console.log(keywords);
+        var date = document.getElementById("datepicker").value;
+        console.log(date);
+        var domaine = document.getElementById("domainlist").value;
+        console.log(domaine)
+        var region = document.getElementById("regionlist").value;
+        console.log(region)
+        var ville = document.getElementById("villeinput").value;
+        var requestOptions = {
+            method: 'GET',
+            redirect: 'follow'
+        };
+
+        fetch("http://localhost:8080/offre/get?Titre=" + keywords + "&Domaine=" + domaine + "&Region=" + region + "&Ville=" + ville + "&Date=" + date, requestOptions)
+            .then(response => response.json())
+            .then(result => {
+                if (result.length == 0) {
+                    setTres("")
+                    setNres("Aucune offre correspond à vos infos")
+                } else if (result.length == 1) {
+                    setTres("Résultat:")
+                    setNres(" 1 offre trouvée")
+                } else {
+                    setTres("Résultat:")
+                    setNres("" +result.length + " offres trouvées")
+                }
+                setData(result)
+            })
+            .catch(error => console.log('error', error));
+        console.log(data)
+    }
+
+    const FindUser =(id) => {
+        var requestOptions = {
+            method: "GET",
+            redirect: "follow",
+        };
+
+        fetch("http://localhost:8080/login/" +id, requestOptions)
+            .then((response) => response.json())
+            .then((res) => {
+                setUser(res)
+            });
+    }
+    return (
+        <div id="Search">
+            <div>
+                <img id="iconglb" src={sch} alt="icon"/>
+            </div>
+            <div id={"searchblock"}>
+                <SearchTextField id={"searchbar"} type={"search"} label="Rechercher" variant="outlined" sx={{ width: '100%' }} onKeyPress={(e)=>{
+                    if(e.key ==="Enter"){
+                        e.preventDefault()
+                        rafraichir()}
+                }}/>
+            </div>
+            <div id={"filter"}>
+                <div id={"regionblock"}>
+                    <label id={"region"}>Région:</label>
+                    <select id={"regionlist"}>
+                        <option>Toutes</option>
+                        <option>Tanger-Tetouan-Al Hoceima</option>
+                        <option>Oriental</option>
+                        <option>Fès-Meknès</option>
+                        <option>Rabat-Salé-Kénitra</option>
+                        <option>Béni Mellal-Khénifra</option>
+                        <option>Casablanca-Settat</option>
+                        <option>Marrakech-Safi</option>
+                        <option>Drâa-Tafilalet</option>
+                        <option>Souss-Massa</option>
+                        <option>Guelmim-Oued Noun</option>
+                        <option>Laâyoune-Sakia El Hamra</option>
+                        <option>Dakhla-Oued Ed-Dahab</option>
+                    </select>
+                </div>
+                <div id={"villeblock"}>
+                    <label id={"ville"}>Ville:</label>
+                    <input id={"villeinput"} type={"text"}></input>
+                </div>
+
+                <div id={"domainblock"}>
+                    <label id={"domain"}>Domaine:</label>
+                    <select id={"domainlist"}>
+                        <option>Toutes</option>
+                        <option>Plombier</option>
+                        <option>Chhiwat</option>
+                        <option>Démenagement</option>
+                        <option>Eléctricien</option>
+                        <option>Jardinage</option>
+                        <option>Dépannages</option>
+                        <option>Mécanicien</option>
+                        <option>Livraison</option>
+                        <option>Autres</option>
+                    </select>
+                </div>
+                <div id={"dateblock"}>
+                    <label id={"date"}>Postée le:</label>
+                    <input id={"datepicker"} type={"date"} min={mindate}/>
+
+                </div>
+            </div>
+            <button id={"search"} onClick={rafraichir}>Rechercher</button>
+
+            <div id={"results"}>
+                <label id={"infores"}>{nres}</label>
+                <div id ={"offres"}>
+                    {data.map((item) => {
+                        return (
+                            <div id={"offer"} key = {item.id} >
+                                <div id="iconcont">
+                                    <img id={"icon"} src={sh}/></div>
+                                <div id={"Titre"}>
+                                    <label id={"title"}>{item["titre"]}</label>
+                                </div>
+                                <div>
+                                    <label id={"place"}>{item["ville"]}</label>
+                                </div>
+                                <label id={"details"} onClick={()=>{setModalShow(true);setI(item);FindUser(item.utilisateur.id)}}>Voir plus</label >
+                                <DetailModal item = {i} user = {user} show={modalShow} onHide={() => setModalShow(false)} />
+
+                            </div>
+                        );
+                    })}
+                </div>
+
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Chercher;
---
+++ src/Chercher/DetailModal/DetailModal.jsx
@@ -0,0 +1,220 @@
+import React, {useState} from "react";
+import Modal from "react-bootstrap/Modal";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
+import "./DetailModal.css"
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import {Alert} from "@mui/material";
+
+function DetailModal(props) {
+    const [validated, setValidated] = useState(false);
+    const [domaine, setDomaine] = useState("");
+    const [duree, setDuree] = useState("");
+    const [prix, setPrix] = useState("");
+    const [description, setDescription] = useState("");
+
+    const postuler = () => {
+        fetch("http://localhost:8080/postulation/post",{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                "prix": prix,
+                "duree": duree,
+                "description": description,
+                "idOffre" : props.item.id,
+                "utilisateur": {
+                    "id" : props.idutilisateur,
+                },
+            }),
+        });
+
+    };
+
+
+    const handleChange5 = (event) => {
+       setDomaine(event.target.value);
+    };
+    const handleChange6 = (event) => {
+        setDescription(event.target.value);
+    };
+    const handleChange7 = (event) => {
+        setDuree(event.target.value);
+    };
+    const handleChange8 = (event) => {
+        setPrix(event.target.value);
+    };
+
+    const handleSubmit= (event)=>{
+        const form = event.currentTarget;
+        if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+        }
+        event.preventDefault();
+        setValidated(true);
+        postuler();
+    }
+
+    const fillblank = (x) => {
+        if (x === "")
+            return "Non affectée"
+        return x
+    }
+
+
+    return (
+        <Modal
+            {...props}
+            size="lg"
+            aria-labelledby="contained-modal-title-vcenter"
+            scrollable={true}
+            centered
+        >
+            <Modal.Header closeButton>
+                <Modal.Title id="contained-modal-title-vcenter">
+                    Detail de "{props.item["titre"]}"
+                </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Container>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Offre</th>
+
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Titre</th>
+                            <td>{props.item["titre"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Description</th>
+                            <td>{props.item["description"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Domaine</th>
+                            <td>{props.item["domaine"]}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Région</th>
+                            <td>{fillblank(props.item["region"])}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Ville</th>
+                            <td>{fillblank(props.item["ville"])}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Date de post</th>
+                            <td>{props.item["dateP"]}</td>
+                        </tr>
+                        </tbody>
+                    </table>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Employeur</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr>
+                            <th scope="row">Nom</th>
+                            <td>{props.user.nom}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Prénom</th>
+                            <td>{props.user.prenom}</td>
+                        </tr>
+                        <tr>
+                            <th scope="row">Télephone</th>
+                            <td>{props.user.tel}</td>
+                        </tr>
+
+                        </tbody>
+                    </table>
+                    <table className="table">
+                    </table>
+                    <table className="table">
+                        <thead>
+                        <tr>
+                            <th scope="col">Postulat</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <Container>
+                            <Row>
+                                <Col>
+                                    <Form noValidate validated={validated} >
+                                        <Form.Group controlId="validationCustom05">
+                                            <Form.Control
+                                                size="xs"
+                                                type="text"
+                                                placeholder="duree"
+                                                onChange={handleChange7}
+                                                required
+                                            ></Form.Control>
+                                            <Form.Control.Feedback type="invalid">
+                                                veuillez entrer une duree
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <br />
+                                        <Form.Group controlId="validationCustom06">
+                                            <Form.Control
+                                                size="xs"
+                                                type="text"
+                                                placeholder="prix"
+                                                onChange={handleChange8}
+                                                required
+                                            ></Form.Control>
+                                            <Form.Control.Feedback type="invalid">
+                                                veuillez entrer un prix
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <br />
+                                        <Form.Group controlId="validationCustom07">
+                                            <Form.Control
+                                                size="xs"
+                                                type="text"
+                                                placeholder="domaine"
+                                                onChange={handleChange5}
+                                                required
+                                            ></Form.Control>
+                                            <Form.Control.Feedback type="invalid">
+                                                veuillez entrer un domaine
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                        <br />
+                                        <Form.Group controlId="validationCustom08">
+                                            <Form.Control
+                                                size="xs"
+                                                as="textarea"
+                                                placeholder="description"
+                                                onChange={handleChange6}
+                                                required
+                                            ></Form.Control>
+                                            <Form.Control.Feedback type="invalid">
+                                                veuillez entrer une description
+                                            </Form.Control.Feedback>
+                                        </Form.Group>
+                                    </Form>
+                                </Col>
+                            </Row>
+                        </Container>
+                        </tbody>
+                    </table>
+                </Container>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button id={"button"} onClick={props.onHide}>Fermer</Button>
+                <Button id ={"button"} onClick={handleSubmit}>Postuler</Button>
+            </Modal.Footer>
+        </Modal>
+);
+}
+
+export default DetailModal;
